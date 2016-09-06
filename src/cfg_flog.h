c93efaa2ccdeb3396727d29c0ea4f5f648062c8c
itahoe <office.tahoe@gmail.com>
2016-09-06 06:15:42 +0300

    Initial commit

---
+++ src/cfg_flog.h
@@ -0,0 +1,16 @@
+/**
+  * @file    cfg_flog.h
+  * @brief   Config - File Logger
+  * @author  Igor T. <research.tahoe@gmail.com>
+  */
+
+#ifndef  CFG_FLOG_H
+#define  CFG_FLOG_H
+
+
+#define CFG_FLOG_BLCK_SIZE_OCT          4096
+
+//#define CFG_FLOG_FILE_SYNC_TOUT_SEC     120
+
+
+#endif	//CFG_FLOG_H
